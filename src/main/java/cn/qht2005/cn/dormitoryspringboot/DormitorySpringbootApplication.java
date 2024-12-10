dcfc07ba083ff33dbb1ef67524e1b3f63c5268c8
qht <qht20050506@outlook.com>
2024-12-10 16:30:35 +0800

    spring boot init

---
+++ src/main/java/cn/qht2005/cn/dormitoryspringboot/DormitorySpringbootApplication.java
@@ -0,0 +1,13 @@
+package cn.qht2005.cn.dormitoryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitorySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormitorySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/qht2005/cn/dormitoryspringboot/DormitorySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qht2005.cn.dormitoryspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitorySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
